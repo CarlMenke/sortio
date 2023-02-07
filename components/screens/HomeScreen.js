664eb258a119d54e70f8526676a520daf085b1c5
carlmenke <carlmenke13@gmail.com>
2023-02-06 19:17:43 -0800

    added returning comments

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -4,6 +4,13 @@ import BusinessCard from '../cards/BusinessCard';
 import { showBusinessForm } from '../../navFunctions'
 import { setNavState } from '../../redux/actions';
 
+
+//NEXT TASKS
+//DYNAMICALLY ADD IN STYLES
+    //figure out how you can add all the styles to one document and then use variables and such for colors and fonts used across multiple
+//add a check for navigation functoins
+//refactor how inventory items are stores in menu items, you use object.keys so you get [key, value] and that messed everything up
+    // go to where you change thre format and prevent it, then go and refactor the rest to make it flow
 export default function HomeScreen() {
     const dispatch = useDispatch()
     const setNavStateAction = (navState) => dispatch(setNavState(navState))
